f7486e598ca38a64e11756760f531a951fc7bb8a
Pavan-Mahesh <150039478+Pavan-Mahesh@users.noreply.github.com>
2023-12-26 22:21:45 +0530

    Initial commit

---
+++ src/ExcelNumbers.java
@@ -0,0 +1,33 @@
+class DecimalToAlpha {
+    private static void printAlpha(int[] bits) {
+        // ASCII from A-Z : 65-90
+        for(int bit: bits)
+            System.out.format("%c", bit==0 ? 90 : 65+bit-1); // If 0 then 'Z'
+        System.out.println();
+    }
+    static void convertToAlpha(int deci) {
+        if(deci <= 0 ) {
+            System.out.println("No Possible answer!");
+            return;
+        }
+        // to find the count of bits (starting from 0)
+        int n_bits = 1, check_value = (int)Math.pow(26, n_bits);
+        while(deci > check_value) {
+            n_bits++;
+            check_value += (int)Math.pow(26, n_bits);
+        }
+        // getting the bits in order
+        int[] bits = new int[n_bits];
+        for(int i=n_bits-1; i>=0; i--) {
+            bits[i] = deci%26;
+            deci = (deci/26) - (deci% 26 == 0 ? 1 : 0);
+        }
+        printAlpha(bits);
+    }
+}
+
+public class ExcelNumbers {
+    public static void main(String[] args) {
+        DecimalToAlpha.convertToAlpha(2730);
+    }
+}
---
+++ src/IntsInDescending.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class IntsInDescending {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("input:");
+        String given = input.nextLine();
+        input.close();
+        String[] num = given.split("\\D+");
+        String[] non_num = given.split("\\d+");
+        if(String.join(" ", non_num).compareTo(given) == 0) {
+            System.out.println("output:");
+            System.out.println(given);
+            return;
+        }
+        boolean empty = num[0].compareTo("") == 0;
+
+        ArrayList<Integer> value = new ArrayList<>();
+        int start = empty ? 1: 0;
+        for(int idx=start; idx<num.length; idx++)
+            value.add(Integer.parseInt(num[idx]));
+        sort(value);
+        for(int idx=0; idx<value.size(); idx++)
+            num[idx+start] = Integer.toString(value.get(idx));
+
+        StringBuilder result = getResult(num, non_num, empty);
+        System.out.println("output:");
+        System.out.println(result);
+    }
+    private static StringBuilder getResult(String[] num, String[] non_num, boolean empty) {
+        StringBuilder result = new StringBuilder();
+        for(int idx = 0; idx<Math.max(num.length, non_num.length); idx++) {
+            if(empty) {
+                result.append(num[idx]);
+                try { result.append(non_num[idx]); } catch (Exception ignore) {}
+            }else {
+                result.append(non_num[idx]);
+                try { result.append(num[idx]); } catch (Exception ignore) {}
+            }
+        }
+        return result;
+    }
+    private static void sort(ArrayList<Integer> value) {
+        for(int i=0; i<value.size()-1; i++) {
+            int max_value_idx = i;
+            for (int j = i; j < value.size(); j++)
+                if (value.get(max_value_idx).compareTo(value.get(j)) < 0)
+                    max_value_idx = j;
+            if (max_value_idx != i) {
+                Integer temp = value.get(i);
+                value.set(i, value.get(max_value_idx));
+                value.set(max_value_idx, temp);
+            }
+        }
+    }
+}
