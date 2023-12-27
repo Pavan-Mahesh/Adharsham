bf5f5afaf29a2e4d27f55edd27774bdb9b3d9cb4
Pavan-Mahesh <150039478+Pavan-Mahesh@users.noreply.github.com>
2023-12-27 20:11:47 +0530

    Update HighestSalary.java

--- src/HighestSalary.java
+++ src/HighestSalary.java
@@ -1,39 +1,59 @@
 import java.util.Scanner;
-import java.util.ArrayList;
 import java.util.regex.*;
 public class HighestSalary {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        Pattern salary_at_end = Pattern.compile("\\b\\d+\\b$");
-        Pattern name_at_begin = Pattern.compile(".+(?=\\b\\d+\\b$)");
-        Matcher m;
+        Pattern salary_pattern = Pattern.compile("\\b\\d+\\b$");
+        Pattern name_pattern = Pattern.compile(".+(?=\\b\\d+\\b$)");
 
-        ArrayList<String> name = new ArrayList<>();
-        ArrayList<Long> salary = new ArrayList<>();
-        String given, s; long l; int idx;
         int iteration = input.nextInt(); input.nextLine();
-        while(iteration > 0) {
+        String[] names = new String[iteration];
+        long[] salaries = new long[iteration];
+        String given, current_name; long current_salary; int idx;
+        for(int i=0; i<iteration; i++) {
             given = input.nextLine();
-            m = name_at_begin.matcher(given);
-            s = m.find() ? (m.group().isBlank() ? "other" : m.group()) : "other";
-            m = salary_at_end.matcher(given);
-            l = m.find() ? Long.parseLong(m.group()) : 0;
-            if(name.contains(s)) {
-                idx = name.indexOf(s);
-                l += salary.get(idx);
-                salary.set(idx, l);
-            } else {
-                name.add(s);
-                salary.add(l);
+            Matcher name_matcher = name_pattern.matcher(given);
+            current_name = name_matcher.find() ? name_matcher.group() : "other";
+            Matcher salary_matcher = salary_pattern.matcher(given);
+            current_salary = salary_matcher.find() ? Long.parseLong(salary_matcher.group()) : 0;
+            idx = checkDuplicates(names, current_name);
+            if(idx != -1)
+                salaries[idx] += current_salary;
+            else {
+                salaries[i] = current_salary;
+                names[i] = current_name;
             }
-            iteration--;
         }
-        input.close();
-
-        long high = salary.getFirst();
-        for(int i=1; i<salary.size(); i++)
-            high = Math.max(high, salary.get(i));
-        idx = salary.indexOf(high);
-        System.out.format("%s %d", name.get(idx), idx);
+        idx = getIndexOfHigh(names, salaries);
+        System.out.println(names[idx] + " " + idx);
+    }
+    static int checkDuplicates(String[] names, String name) {
+        name = name.replaceAll(" ", "");
+        for(int i=0; i<names.length; i++) {
+            if(names[i] == null)
+                return -1;
+            if((names[i].replaceAll(" ", "")).equals(name))
+                return i;
+        }
+        return -1;
+    }
+    static int getIndexOfHigh(String[] names, long[] salaries) {
+        int idx = 0;
+        long high = salaries[idx];
+        for(int i=1; i<salaries.length; i++) {
+            if(high > salaries[i])
+                continue;
+            boolean update = true;
+            if(high == salaries[i]) {
+                String previous = names[idx].replaceAll(" ", "");
+                String current = names[i].replaceAll(" ", "");
+                update = previous.compareTo(current) > 0;
+            }
+            if(update) {
+                high = Math.max(high, salaries[i]);
+                idx = i;
+            }
+        }
+        return idx;
     }
 }
